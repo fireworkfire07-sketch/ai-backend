9218399531fd815676d0b890d793209089106df1
fireworkfire07-sketch <fireworkfire07@hotmail.com>
2026-05-05 17:00:52 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,3 +1,26 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "çalışıyor" });
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization":`Bearer ${process.env.OPENAI_API_KEY},
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        model: "gpt-4.1-mini",
+        messages: [
+          { role: "user", content: "Selam" }
+        ]
+      })
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+
+  } catch (error) {
+    res.status(500).json({
+      error: "Hata oluştu",
+      detail: error.message
+    });
+  }
 }
