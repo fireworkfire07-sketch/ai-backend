37d66d16d7b1291dbdb607c52963ac72e2382b9b
fireworkfire07-sketch <fireworkfire07@hotmail.com>
2026-05-08 23:47:12 +0300

    Fix error message from Turkish to English

--- api/chat.js
+++ api/chat.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
 
     if (!message) {
       return res.status(400).json({
-        error: "Mesaj gerekli"
+        error: "Message gerekli"
       });
     }
 
